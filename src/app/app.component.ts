0aa50c00bcc4bd66e3fdf2b6c8477e85f179015d
Jhon Florez <jhon.florezr@gmail.com>
2025-02-08 16:11:32 +0100

    added Header Html structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+  HeaderComponent,
+  FooterComponent,
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

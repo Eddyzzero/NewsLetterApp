d768682e1a9d53648f7570d2f1f97f653983d3bf
Jhon Florez <jhon.florezr@gmail.com>
2025-02-17 19:57:04 +0100

    added categories: components, routes, services,

---
+++ src/app/features/entertainment/entertainment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entertainment',
+  imports: [],
+  templateUrl: './entertainment.component.html',
+  styleUrl: './entertainment.component.scss'
+})
+export class EntertainmentComponent {
+
+}
